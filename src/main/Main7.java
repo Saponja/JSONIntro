65ec010be3c4a58b42641fb49c77aab7a1831b10
Saponja <jsaponjic512@gmail.com>
2021-03-29 15:41:26 +0200

    Json sa web servisa, ispravljen base_url

--- src/main/Main7.java
+++ src/main/Main7.java
@@ -10,7 +10,7 @@ import com.google.gson.JsonObject;
 
 public class Main7 {
 	
-	private static final String BASE_URL = "http://currencylayer.com";
+	private static final String BASE_URL = "http://api.currencylayer.com";
 	private static final String API_KEY = "2e4baadf5c5ae6ba436f53ae5558107f";
 	private static final String SOURCE = "USD";
 	private static final String CURRENCIES = "EUR";
